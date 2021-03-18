a532982cf4e4f2fb3b21b6bb801a3b70c2b5927f
dingwenchao <dingwenchao@shuame.com>
2021-03-18 20:17:53 +0800

    update ES6继承

--- class/inherit.js
+++ class/inherit.js
@@ -6,6 +6,10 @@
  */
 class Super {
   x = 1
+  static testArray = [1]
+  constructor() {
+    debugger
+  }
   static log(msg) {
     console.log('message', msg)
   }
@@ -15,6 +19,8 @@ class Super {
 }
 
 class Sub extends Super {
+  y = 2
+  static testArray = [3, 1]
   constructor() {
     super()
   }
@@ -22,26 +28,81 @@ class Sub extends Super {
     super.log('test super on class')
   }
   testSuperOnInstance() {
+    console.log(super.x)
+    // super()
     super.test('test suepr on instance')
+  }
+}
+
+class SubB extends Super {
+  constructor() {
+    super()
   }
 }
 
 
 Sub.testSuperOnClass()
-const superInstance = new Super()
+console.log('sub test Array', Sub.testArray)
+Sub.testArray.push(2)
+console.log('sub test Array', Sub.testArray)
+
+console.log('super test Array', SubB.testArray)
+// const superInstance = new Super()
 const sub = new Sub()
 sub.testSuperOnInstance()
 /**
- * ES6的继承链有两条
+ * ES6的继承链有三条
  * 一个是静态属性的继承也就是以父类自身为原型创建父类
  * 二是原型继承也就是Sub.prototype = Object.create(Super.prototype)
  * 另外还有一条隐藏的继承链是通过super()实现实例属性的继承的
  * 所以得出结论ES6的继承只不过是之前组合继承的语法糖
  */
 console.log(Sub.__proto__ === Super)
+console.log(Sub.constructor)
 console.log(sub.__proto__ === Sub.prototype)
 // 也就是Sub.prototype = Object.create(Super.prototype)
 console.log(Sub.prototype.__proto__ === Super.prototype)
-console.log(sub.hasOwnProperty('x'))
+console.log('sub has own property x?', sub.hasOwnProperty('x'))
 // 方法存在于原型上
 console.log(sub.hasOwnProperty('testSuperOnInstance'))
+
+/**
+ * ES5寄生组合式继承实现
+ */
+
+function Super() {
+  this.x = 1
+}
+
+Super.testArray = [1]
+
+Super.log =  function(msg) {
+  console.log('message', msg)
+}
+
+Super.prototype.test = function(msg) {
+  console.log('test message', msg)
+}
+
+function Sub() {
+  // 盗用构造函数继承实例属性
+  Super.apply(this)
+  this.y = 2
+}
+
+// 继承静态属性
+Sub.__proto__ = Super
+
+Sub.testSuperOnClass = function() {
+  super.log('test super on class')
+}
+
+// 原型继承实现方法的继承
+var p = Object.create(Super.prototype)
+p.constructor = Sub
+p.testSuperOnInstance = function() {
+  console.log(super.x)
+  // super()
+  super.test('test suepr on instance')
+}
+Sub.prototype = p
